a0fa140ebd66c365ab9560263e08e66384933980
Bhavya Dudhatra <bhavyadudhatrahere@example.com>
2025-11-10 22:52:26 +0530

    pattern4

---
+++ Patterns/p4.java
@@ -0,0 +1,19 @@
+// Character pattern
+
+// A
+// BC
+// DEF
+// GHIJ
+
+public class p4{
+    public static void main(String args[]){
+        int a=65;
+        for(int i=1;i<5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((char)a);
+                a++;
+            }
+            System.out.println();
+        }
+    }
+}
