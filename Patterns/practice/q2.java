2cadf33ff55b16c91506032a0993a8d14dd81384
Bhavya Dudhatra <bhavyadudhatrahere@example.com>
2025-11-12 18:33:36 +0530

    Inverted and Rotated Half-Pyramid pattern

---
+++ Patterns/practice/q2.java
@@ -0,0 +1,24 @@
+// Inverted and Rotated Half-Pyramid pattern
+
+//      *
+//     **
+//    ***
+//   ****
+//  *****
+// ******
+
+public class q2{
+    public static void main(String args[]){
+        int no=5;
+        for(int i=1;i<=no;i++){
+            for(int j=1;j<=no-i;j++){
+                System.out.print("   ");
+                
+            }
+            for(int k=1;k<=i;k++){
+                    System.out.print(" * ");
+                }
+            System.out.println();
+        }
+    }
+}
