8427e3482d2c0efb47b94e83d3069ffeff840094
Bhavya Dudhatra <bhavyadudhatrahere@example.com>
2025-11-22 22:48:44 +0530

    'patern5'

---
+++ Patterns/p5.java
@@ -0,0 +1,19 @@
+// Alphabet pattern
+
+// A 
+// B B 
+// C C C 
+// D D D D
+
+public class p5{
+    public static void main(String[] args) {
+        char c='A';
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(c);
+            }
+            System.out.println();
+            c++;
+        }
+    }
+}
