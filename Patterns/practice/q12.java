d3120341754313a886485a3755ebd8290ae5abc9
Bhavya Dudhatra <bhavyadudhatrahere@example.com>
2025-11-30 22:33:32 +0530

    pattern q12 q13

---
+++ Patterns/practice/q12.java
@@ -0,0 +1,17 @@
+// Hollow Rectangle
+
+public class q12 {
+    public static void main(String[] args) {
+        int no=5;
+        for(int i=0;i<=no;i++){
+            for(int j=0;j<=no;j++){
+                if(i==0 || i==no || j==0 ||j==no){
+                    System.out.print(" * ");
+                }else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/practice/q13.java
@@ -0,0 +1,13 @@
+// triangle
+
+public class q13 {
+    public static void main(String[] args) {
+        int no=5;
+        for(int i=0;i<=no;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
